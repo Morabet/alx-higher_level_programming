bcb37549c76b4bb2cbf3c309d407edd28efc1f61
Morabet <ismailelmorabet2@gmail.com>
2023-09-05 11:02:13 -0700

    task 0 to 5

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+ld = number % 10 if number > 0 else (-number) % 10
+
+if number < 0:
+    ld *= -1
+
+print(f"Last digit of {number} is {ld} and is ", end="")
+
+if ld > 5:
+    print("greater than 5")
+elif ld == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for ch in range(ord('a'), ord('z') + 1):
+    if ch != ord('q') and ch != ord('e'):
+        print("{}".format(chr(ch)), end="")
