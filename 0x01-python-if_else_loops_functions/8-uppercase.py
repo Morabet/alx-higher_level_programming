26da5ad10413763b8daca226673ce906b43664f0
Morabet <ismailelmorabet2@gmail.com>
2023-09-05 11:28:14 -0700

    last task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
 
     for c in str:
         x = ord(c) in range(ord('a'), ord('z') + 1)
-         print("{}".format(chr(ord(c) - ord(' ')) if x else c), end="")
+        print("{}".format(chr(ord(c) - ord(' ')) if x else c), end="")
     else:
         print()
