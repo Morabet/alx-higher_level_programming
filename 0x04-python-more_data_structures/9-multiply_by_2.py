755451118fb2198044ee9847341cf8e8e2f47383
Morabet <ismailelmorabet2@gmail.com>
2023-09-13 10:43:43 -0700

    fix 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,5 @@
 
 def multiply_by_2(a_dictionary):
 
-    return {key: (lambda x: x * 2)(value) for key, value in a_dictionary.items()}
+    return {key: (lambda x: x * 2)(value) \
+            for key, value in a_dictionary.items()}
