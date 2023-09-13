8869ed22e3bbf090cbda48932ae8f720d2958c8e
Morabet <ismailelmorabet2@gmail.com>
2023-09-13 12:13:18 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
 
-#from functools import reduce
-
 
 def roman_to_int(roman_string):
     dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     values = [dict[char] for char in roman_string]
+    summ = 0
 
     if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
 
     if len(roman_string) == 1:
         for value in values:
-            sum += value
-       # sum = reduce(lambda x, y: x + y, values)
+            summ += value
     if roman_string[0] > roman_string[-1]:
         for value in values:
-            sum += value
-        #sum = reduce(lambda x, y: x + y, values)
+            summ += value
     if roman_string[0] == roman_string[-1]:
         for value in values:
-            sum += value
-       # sum = reduce(lambda x, y: x + y, values)
+            summ += value
     if roman_string[0] < roman_string[-1]:
         for value in reversed(values):
-            sum -= value
-        #sum = reduce(lambda x, y: x - y, reversed(values))
+            summ -= value
 
-    return sum
+    return summ
