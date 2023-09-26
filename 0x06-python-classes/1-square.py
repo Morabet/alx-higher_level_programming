b77bb246cd2178bf4540483bced5a82d42037ad2
Morabet <ismailelmorabet2@gmail.com>
2023-09-26 10:18:53 -0700

    task 0 to 6

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module that defines a square """
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        """
+
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""A module that defines a square """
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter for the size attribute"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for the size attribute"""
+
+        self.__init__(value)
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The the area of the square
+        """
+        return self.__size ** 2
