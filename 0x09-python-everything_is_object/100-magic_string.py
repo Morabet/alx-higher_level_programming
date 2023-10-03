df6dcf4c23cc4c95c4a3b923a874ab756e1d8bd5
Morabet <ismailelmorabet2@gmail.com>
2023-10-03 11:19:15 -0700

    tasks 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c = getattr(magic_string,"c" , 0) + 1
+    magic_string.c = getattr(magic_string, "c", 0) + 1
     return ", ".join(["BestSchool"] * magic_string.c)
