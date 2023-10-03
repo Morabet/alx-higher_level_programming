c6fb480aaae2db5c17f97d3a96ac4875e0bcd210
Morabet <ismailelmorabet2@gmail.com>
2023-10-03 11:13:47 -0700

    tasks 100 and 101

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/use/bin/python3
+def magic_string():
+    magic_string.c = getattr(magic_string,"c" , 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.c)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" define 101-locked_class Module """
+
+
+class LockedClass:
+    """ class of locked class """
+    #  define the available slots that can be created
+    __slots__ = ('first_name')
