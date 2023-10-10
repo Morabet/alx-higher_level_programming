3a1f0ad04502ac6500b49cdf383d319837f75122
Morabet <ismailelmorabet2@gmail.com>
2023-10-10 15:11:33 -0700

    fix 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,4 +7,15 @@ def pascal_triangle(n):
     representing the Pascal’s triangle of n
     """
 
+    if n <= 0:
+        return []
 
+    tri_pascal = [[1]]
+    while len(tri_pascal) != n:
+        tri = tri_pascal[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        tri_pascal.append(tmp)
+    return tri_pascal
