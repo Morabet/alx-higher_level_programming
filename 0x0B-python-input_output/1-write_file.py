4ee18b1050013157da8c7fb1ab46c407eb2ac07e
Morabet <ismailelmorabet2@gmail.com>
2023-10-10 14:17:39 -0700

    fix 1 7

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,4 @@
-/usr/bin/python3
+#!/usr/bin/python3
 """Defining the '1-write_file' module"""
 
 
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,14 +11,14 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = argv[1:]
-file = "add_item.json"
+filename = "add_item.json"
 
 try:
-    old_list = load_from_json_file(file)
+    old_list = load_from_json_file(filename)
 
 except FileNotFoundError:
     old_list = []
 
 old_list.extend(args)
 
-save_to_json_file(old_list, file)
+save_to_json_file(old_list, filename)
