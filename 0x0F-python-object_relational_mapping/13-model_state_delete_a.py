ed17250eec9b2b09695924d8e31e0d5647a10533
Morabet <ismailelmorabet2@gmail.com>
2023-12-17 14:10:07 -0800

    13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-changes the name of the State object where id=2 to New Mexico from a database
+a script that deletes all 'State objects' with a 'name'
+containing the letter 'a' from the database hbtn_0e_6_usa
 """
 
 from sqlalchemy import create_engine
