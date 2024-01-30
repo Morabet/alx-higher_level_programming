bb2c4246abdbe384c9be6b1559f8ef7a52900a5d
Morabet <ismailelmorabet2@gmail.com>
2024-01-30 10:49:28 -0800

    task 5 fixed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,7 +5,5 @@ const argv = require('process').argv;
 const fs = require('fs');
 
 if (argv.length >= 4) {
-  request.get(argv[2], 'utf8', err => {
-    console.log(err);
-  }).pipe(fs.createWriteStream(argv[3]));
+  request(argv[2]).pipe(fs.createWriteStream(argv[3]));
 }
